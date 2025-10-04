20e8b59bdb9047cf255c8090d21e1436ed54ad53
xDuinoRails <156117281+xDuinoRails@users.noreply.github.com>
2025-10-04 23:03:48 +0200

    Update neopixel_pio.py

--- neopixel_pio.py
+++ neopixel_pio.py
@@ -2,35 +2,47 @@ import array, time
 from machine import Pin
 import rp2
 
-# PIO program for WS2812
-@rp2.asm_pio(sideset_init=rp2.PIO.OUT_LOW, out_shiftdir=rp2.PIO.SHIFT_LEFT, autopull=True, pull_thresh=24)
-def ws2812():
-    T1 = 2
-    T2 = 5
-    T3 = 3
-    wrap_target()
-    label("bitloop")
-    out(x, 1)           .side(0) [T3 - 1]
-    jmp(not_x, "do_zero").side(1) [T1 - 1]
-    jmp("bitloop")      .side(1) [T2 - 1]
-    label("do_zero")
-    nop()               .side(0) [T2 - 1]
-    wrap()
-
 NUM_LEDS = 8
-PIN_NUM = 16
+PIN_NUM  = 0
 
-# State machine setup
-sm = rp2.StateMachine(0, ws2812, freq=8000000, sideset_base=Pin(PIN_NUM))
+# PIO program for WS2812
+@rp2.asm_pio( set_init=rp2.PIO.OUT_LOW
+            , out_init=rp2.PIO.OUT_LOW
+            , out_shiftdir=rp2.PIO.SHIFT_LEFT
+            , autopull=True
+            , pull_thresh=24)
+def ws2812():
+    set(pins, 0)   [2]  # 3 cycles = 375ns 'LOW'
+    set(pins, 1)   [1]  # 2 cycles = 250ns 'HIGH'
+    out(pins, 1)   [4]  # 5 clocks = 675ns DATA (HIGH or LOW)
+
+# 
+# Frequency choice
+# - Neopixel requires 800kHz per bit
+# - Let's use 10 cycle per bit 
+#      = 8MHz frequency
+#      ~ 125ns per cycle
+#
+# - Use both "out" and "set" for the simple code
+# 
+sm = rp2.StateMachine( 0, ws2812, freq=8_000_000
+                     , set_base=Pin(PIN_NUM)
+                     , out_base=Pin(PIN_NUM)
+                     )
 sm.active(1)
 
 def show(colors):
     ar = array.array("I", colors)
     sm.put(ar, 8)
 
+def show_n_sleep(color):
+    show(color*NUM_LEDS)  # Green
+    time.sleep(0.6)
+
 # Example usage
 while True:
-    show([0x100000]*NUM_LEDS)  # Red
-    time.sleep(0.5)
-    show([0x001000]*NUM_LEDS)  # Green
-    time.sleep(0.5)
+    show_n_sleep([0xFF0000])  # Green
+    show_n_sleep([0x00FF00])  # Red
+    show_n_sleep([0x0000FF])  # Blue
+    show_n_sleep([0x000000])  # Black
+
